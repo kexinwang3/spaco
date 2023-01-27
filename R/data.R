b2af170900b5df9c85effa2b52d87f6b5c491c1f
kexinwang <kexin.wang.kw789@yale.edu>
2023-01-27 13:23:52 -0500

    update data description

--- R/data.R
+++ R/data.R
@@ -1,43 +1,43 @@
-#' impact_imputed
-#'
-#' impact_imputed
-#'
+#' @title Imputed IMPACT Dataset
+#' @description IMPACT dataset after imputing missing values.
 #' @details
-#' `time` is the time of the measurement; \cr
-#' `X` acceleration in the "X" direction; \cr
-#' `Y` acceleration in the "Y" direction; \cr
-#' `Z` acceleration in the "Z" direction;
-#'
+#' See \href{https://pubmed.ncbi.nlm.nih.gov/32717743/}{IMPACT Study} for more
+#' information.
+#' The dataset consists of 180 samples across 135 immunological features
+#' collected from 98 participants infected with COVID-19. Viral RNA load,
+#' levels of plasma cytokines and chemokines, and leukocyte profiles were
+#' assessed in the study. Follow-up measurements of the patients included 1 to
+#' 7 longitudinal time points, occurring 3-51 days after symptom onset.
 #' @name impact_imputed
 #' @docType data
 #' @keywords data
 NULL
 
-#' impact_missing
-#'
-#' impact_missing
-#'
+#' @title Missing IMPACT Dataset
+#' @description IMPACT dataset with missing values.
 #' @details
-#' `time` is the time of the measurement; \cr
-#' `X` acceleration in the "X" direction; \cr
-#' `Y` acceleration in the "Y" direction; \cr
-#' `Z` acceleration in the "Z" direction;
-#'
+#' See \href{https://pubmed.ncbi.nlm.nih.gov/32717743/}{IMPACT Study} for more
+#' information.
+#' The dataset consists of 180 samples across 135 immunological features
+#' collected from 98 participants infected with COVID-19. Viral RNA load,
+#' levels of plasma cytokines and chemokines, and leukocyte profiles were
+#' assessed in the study. Follow-up measurements of the patients included 1 to
+#' 7 longitudinal time points, occurring 3-51 days after symptom onset.
 #' @name impact_missing
 #' @docType data
 #' @keywords data
 NULL
 
-#' immune
-#'
-#' immune
-#'
+#' @title Original IMMUNE Dataset
+#' @description IMMUNE dataset retrieved from the original source.
 #' @details
-#' `time` is the time of the measurement;
-#' `X` acceleration in the "X" direction;
-#' `Y` acceleration in the "Y" direction;
-#' `Z` acceleration in the "Z" direction;
-#'
+#' See \href{https://pubmed.ncbi.nlm.nih.gov/33361110/}{IMMUNE Profiling Study}
+#' for more information.
+#' The dataset contains 106 samples of 170 immunological observations collected
+#' from 45 participants infected with COVID-19. Blood samples of patients were
+#' collected at enrollment and approximately every 7 days thereafter. The
+#' ratios of different immune cells in the blood test were measured at these
+#' time points.
 #' @name immune_original
 #' @docType data
 #' @keywords data
